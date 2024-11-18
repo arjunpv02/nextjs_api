d7f1e51d3e153462546717e5bc2b494edaf4f898
ARJUN P V <125240263+arjunpv02@users.noreply.github.com>
2024-11-18 11:17:52 +0530

    Create index(page).js

---
+++ index(page).js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <a href="/login">Login</a>
+            <br />
+            <a href="/hello-world">Hello World</a>
+        </div>
+    );
+}
